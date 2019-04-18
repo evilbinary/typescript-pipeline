326e589e5db84709a6e13a28142050bd265f24f3
evilbinary <rootntsd@gmail.com>
2019-04-18 10:42:17 +0800

    remove log

--- src/object.ts
+++ src/object.ts
@@ -37,22 +37,14 @@ export class OObject {
     toObject: boolean = false
   ): any {
     const matches = OObject.getMatches(format, regexObject);
-    console.log('matches', matches);
-    console.log('arg', arg);
-
     const mret = matches.map(o => {
       const exp = o[1].split('|');
-      console.log('exp', exp);
-
       const retExp = exp.map(obj => {
-        console.log('obj=>', obj, 'arg=>', arg);
         if (obj === '.') {
           return arg;
         }
         return deepGet(arg, obj, obj);
       });
-
-      console.log('retExp=>', retExp);
       const result = retExp.reduce((p, k) => {
         // console.log('p=', p, 'k=', k);
         const pret = processPipe(p, k);
@@ -63,16 +55,12 @@ export class OObject {
       //args.reduce(()=>);
     });
     try {
-      console.log('-->', format);
       matches.forEach((m, i) => {
-        console.log('m[0]=>', m[0], 'mret=>', JSON.stringify(mret[i]));
         format = format.replace(m[0], JSON.stringify(mret[i]));
       });
-      console.log('-->format=>', format);
       if (toObject) {
-        console.log('==>', JSON.parse(format));
         return JSON.parse(format);
-      }else{
+      } else {
         return format;
       }
     } catch (e) {
